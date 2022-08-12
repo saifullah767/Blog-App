1d004e876b32c800863dbfe610283e4169585aad
unknown <skj99667788@gmail.com>
2022-08-12 12:27:37 +0500

    update comment counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,13 @@ class Comment < ApplicationRecord
 
   # method that updates the comments counter for a post.
 
+
+  after_save :update_comments_counter
+
+  private
+
   def update_comments_counter
-    post.update(comments_counter: post.comments.count)
+    post.increment!(:comments_counter)
   end
+
 end
