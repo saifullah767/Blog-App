134859048e63059d9f4b26911b8a265a7f332f53
unknown <skj99667788@gmail.com>
2022-08-18 21:45:11 +0500

    routes and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
-  root "users#index" 
-  get '/login', to: 'login#index'
-  get "/users/:user_id", to: "users#show" 
-  get "/users/:user_id/posts", to: "posts#index" 
-  get "/users/:user_id/posts/:id", to: "posts#show"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
+  end
+  resources :login, only: [:index, :show] do
+
+  # root "users#index" 
+  # get '/login', to: 'login#index'
+  # get "/users/:user_id", to: "users#show" 
+  # get "/users/:user_id/posts", to: "posts#index" 
+  # get "/users/:user_id/posts/:id", to: "posts#show"
 end
