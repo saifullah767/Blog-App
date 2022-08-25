dcb239f186254db108d1b0f0c9c28707549e8156
unknown <skj99667788@gmail.com>
2022-08-26 01:04:19 +0500

    Homelink update

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'home/index'
-  root to: "home#index"
-  devise_for :users
+  # get 'home/index'
+  # root to: "home#index"
+  # devise_for :users
  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+
+  root "home#index"
+  devise_for :users
 
   
   resources :users, only: [:index, :show] do
