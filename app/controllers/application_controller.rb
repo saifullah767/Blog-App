75a5fdae98f755c0735e9c58a720a292ad9a06e3
unknown <skj99667788@gmail.com>
2022-08-25 23:58:10 +0500

    Sign up implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # def current_user
   #   User.first
   # end
+
+  protect_from_forgery with: :null_session
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :photo ])
+  end
+  
 end
